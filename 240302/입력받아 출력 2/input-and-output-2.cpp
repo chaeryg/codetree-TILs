4b8825c064ccdc751158b1846529e64499208f2e
chaeryg <74517443+chaeryg@users.noreply.github.com>
2024-03-02 21:24:27 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int birth;
+    int number;
+    char d;
+
+    cin >> birth >> d >> number;
+    cout << birth << number;
+    return 0;
+}
