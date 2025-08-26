2fd970dce13218a0733e6cb56525ef07133b55a1
chaeryg <74517443+chaeryg@users.noreply.github.com>
2025-08-26 21:55:37 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250826/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << "12345^&*()_" << endl;
+    }
+}
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    print(N);
+    
+    return 0;
+}
